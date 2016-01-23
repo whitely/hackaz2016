e195c98830e5cc08562994b6b540b27a8c5c45eb
Patrick Callahan <patrick.callahan@latitudeengineering.com>
2016-01-23 15:21:07 -0700

    add likely deps to project.clj

--- gengen/project.clj
+++ gengen/project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [ 
+                  [org.clojure/clojure "1.8.0"]
+                  [ubergraph "0.1.9"]
+                  [instaparse "1.4.1"]
+                  [rhizome "0.2.5"]
+                  [aysylu/loom "0.5.4"]
+                  [org.clojure/core.logic "0.8.10"]
+                  [jiraph "0.8.2"]
+                ]
   :main ^:skip-aot gengen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
