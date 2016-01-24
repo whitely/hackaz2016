99c866349c67912ca484caaa7f5b2beb1fab09a5
Patrick Callahan <patrick.callahan@latitudeengineering.com>
2016-01-23 19:47:04 -0700

    bazzic ass template for rapid repl dev

--- gengen/src/gengen/core.clj
+++ gengen/src/gengen/core.clj
@@ -1,7 +1,13 @@
 (ns gengen.core
+  (:require [instaparse.core :as insta])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def depscan (insta/parser "resources/pw-dep.ebnf"))
+
+(insta/parse depscan (slurp "resources/pw-dep.ebnf"))
+
