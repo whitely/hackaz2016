b5a325d024c19e49eaf1962f5b9847074dc7405d
Patrick Callahan <patrick.callahan@latitudeengineering.com>
2016-01-24 06:47:47 -0700

    ih8badgrammar

--- gengen/project.clj
+++ gengen/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.gnu.org/licenses/agpl.html"}
   :dependencies [ [org.clojure/clojure "1.8.0"]
                   [instaparse "1.4.1"]
+                  [rhizome    "0.2.5"]
                   [ubergraph "0.1.9"]]
   :main ^:skip-aot gengen.core
   :target-path "target/%s"
